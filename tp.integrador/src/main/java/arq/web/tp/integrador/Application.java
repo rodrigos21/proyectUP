e30fed828c455b9a1e3d3c4db2ba2c616b6016c6
rsanchis <rodrigosanchis1@gmail.com>
2024-10-30 02:06:00 +0100

    remove logs

--- tp.integrador/src/main/java/arq/web/tp/integrador/Application.java
+++ tp.integrador/src/main/java/arq/web/tp/integrador/Application.java
@@ -7,23 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		var var1 = System.getenv("DB_URL");
-		var var2 = System.getenv("DB_USERNAME");
-		var var3 = System.getenv("DB_PASSWORD");
-		var var4 = System.getenv("MYSQL_ROOT_PASSWORD");
-		var var5 = System.getenv("MYSQL_PASSWORD");
-		var var6 = System.getenv("MYSQL_USER");
-		var var7 = System.getenv("MYSQL_DATABASE");
-
-		System.out.println("######### INIT PRINTS ########## ");
-		System.out.println("DB_URL: " + var1);
-		System.out.println("DB_USERNAME: " + var2);
-		System.out.println("DB_PASSWORD: " + var3);
-		System.out.println("MYSQL_ROOT_PASSWORD: " + var4);
-		System.out.println("MYSQL_PASSWORD: " + var5);
-		System.out.println("MYSQL_USER: " + var6);
-		System.out.println("MYSQL_DATABASE: " + var7);
-		System.out.println("######### END PRINTS ########## ");
 		SpringApplication.run(Application.class, args);
 	}
 
