c5e757b60f25e64109c9038d5090845933b36bb3
rsanchis <rodrigosanchis1@gmail.com>
2024-11-01 10:43:25 +0100

    add sentry

--- tp.integrador/src/main/java/arq/web/tp/integrador/Application.java
+++ tp.integrador/src/main/java/arq/web/tp/integrador/Application.java
@@ -1,5 +1,6 @@
 package arq.web.tp.integrador;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		Sentry.init();
 		SpringApplication.run(Application.class, args);
 	}
 
